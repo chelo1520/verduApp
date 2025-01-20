9722de5e83cafab158240192b2c936c8ada39c6e
Marcelo <becerramarcelo002@gmail.com>
2025-01-20 09:54:41 -0300

    Rutas y Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Home } from './pages/Home'
+import { Cart } from './pages/Cart'
+import { Navbar } from './components/NavBar/NavBar'
+import { About } from './pages/About'
 
 function App() {
 
 
   return (
-    <>
-
-    </>
+    <BrowserRouter>
+      <div className='app'>
+          <Navbar/>
+          <div className='main-content container'>
+            <Routes>
+              <Route path='/' element={<Home/>}/>
+              <Route path='/cart' element={<Cart/>}/>
+              <Route path='/about' element={<About/>}/>
+            </Routes>
+          </div>
+      </div>
+    </BrowserRouter>
   )
 }
 
---
+++ src/components/NavBar/Navbar.jsx
@@ -0,0 +1,26 @@
+import { NavLink } from "react-router-dom"
+
+
+export const Navbar = () => {
+  return (
+    <div className="conatiner nav-container">
+      <ul className="nav">
+
+        <li className="nav-item">
+          <NavLink to="/" className={({isActive}) => `nav-link ${isActive ? "active" : ""}`}>
+            <i className="bi bi-shop"></i>
+          </NavLink>
+        </li>
+
+        <li className="nav-item">
+          <NavLink to="/cart" className={({isActive}) => `nav-link ${isActive ? "active" : ""}`}><i className="bi bi-cart2"></i></NavLink>
+        </li>
+
+        <li className="nav-item">
+          <NavLink to="/about" className={({isActive}) => `nav-link ${isActive ? "active" : ""}`}><i className="bi bi-file-earmark-person"></i></NavLink>
+        </li>
+
+      </ul>
+    </div>
+  )
+}
